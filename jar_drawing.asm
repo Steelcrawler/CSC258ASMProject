5a1ab40238fc43d8158294f15d73d4c0f632e4b2
Steelcrawler <umailadi@gmail.com>
2024-11-12 19:03:30 -0500

    added top line

--- jar_drawing.asm
+++ jar_drawing.asm
@@ -8,34 +8,39 @@ li $t4, 0xffffff
     
 add $t5, $zero, $zero
 addi $t5, $t5, 256
-addi $t6, $zero, 640 # end value for outer loop
 
-row_loop:
+lip_loop:
     add $t7, $t0, $t5       # Get current row address (base + row offset)
     sw $t4, 52($t7)          # Draw at first column (offset 0)
     sw $t4, 68($t7)          # Draw at second column (offset 4)
     addi $t5, $t5, 128      # Move to next row (add 256 to get to next row)
-    beq $t5, $t6, wider_vase       # If we hit 640, we're done
-    j row_loop              # Otherwise, continue loop
+    beq $t5, 640, wider_vase       # If we hit 640, we're done
+    j lip_loop              # Otherwise, continue loop
 
 wider_vase:
     add $t8, $zero, $zero   
     addi $t8, $t8, 640      # Start at row 640
-    addi $t6, $zero, 2688   # New end value
 wider_loop:
     add $t7, $t0, $t8       # Get current row address
     sw $t4, 24($t7)        
     sw $t4, 96($t7)        
     
     addi $t8, $t8, 128      
-    beq $t8, $t6, top_line_draw       
+    beq $t8, 2688, top_line_draw       
     j wider_loop            
     
 top_line_draw:
-    add $t8, $zero, $zero   
-    addi $t8, $t8, 640
-top_line_loop:
-    add $t7, $t0, $t8
-    beq $8
+add $t5, $zero, $zero      # Initialize offset to zero
+addi $t7, $zero, 96       # Set final value (where we want to stop)
+addi $t8, $zero, 640       # Row offset value
+add $t6, $t0, $t8          # Base address + row offset (gets us to correct row)
+addi $t2, $zero, 24        # Start at column 48
 
-END:
+top_line_start:
+add $t3, $t6, $t2          # Calculate current pixel position
+sw $t4, 0($t3)             # Draw pixel at current position
+addi $t2, $t2, 4           # Move to next pixel position
+beq $t2, $t7, top_line_end # If we've reached the end, break
+j top_line_start           # Otherwise, continue drawing
+
+top_line_end:
